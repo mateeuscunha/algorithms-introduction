af6600980f715b3b3bbd319813f5752091daaa52
Mateus Cunha <mateuscunhazp@gmail.com>
2026-03-24 20:26:48 -0300

    pre incremento

---
+++ aula5/pre-incremento.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x = 2;
+	int y = 3;
+	int z = 0;
+	
+	z = ++x + ++y;
+	
+	cout << "x = " << x << endl;
+	cout << "y = " << y << endl;
+	cout << "z = " << z << endl;
+	
+	return 0;
+}
