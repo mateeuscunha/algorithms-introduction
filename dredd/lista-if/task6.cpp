67a77c4bb4441fee06f2154e03df9b90a9f41d92
Mateus Cunha <mateuscunhazp@gmail.com>
2026-04-11 19:01:43 -0300

    saving a code with error

---
+++ dredd/lista-if/task6.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float peso, altura;
+    char classe;
+    cin >> peso >> altura;
+
+    //primeira linha da tabela:
+    if (altura < 1.2) {
+        if (peso < 60) {
+            classe = 'A';
+        } else if (peso <= 90) {
+            classe = 'D';
+        } else {
+            classe = 'G';
+        }
+    //segunda linha da tabela:
+    } else if (altura <= 1.7) { 
+        if (peso < 60) {
+            classe = 'B';
+        } else if (peso <= 90) {
+            classe = 'E';
+        } else {
+            classe = 'H';
+        }
+    //terceira linha da tabela:
+    } else {
+        if (peso < 60) {
+            classe = 'C';
+        } else if (peso <= 90) {
+            classe = 'F';
+        } else {
+            classe = 'I';
+        }
+    }
+    
+    cout << classe << endl;
+    
+    return 0;
+}
