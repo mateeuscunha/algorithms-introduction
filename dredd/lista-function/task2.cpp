5ee9da4dc47bf08f26fc208ec7a118f980beae50
Mateus Cunha <mateuscunhazp@gmail.com>
2026-04-12 17:29:11 -0300

    task 2 function list

---
+++ dredd/lista-function/task2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+float inclinacao(int altura, int base) {
+    float res = (altura * 100) / base;
+    return res;
+}
+
+int main() {
+    int qtd, altura, base, contador, adequadas;
+    cin >> qtd;
+    contador = 0;
+    adequadas = 0;
+
+    while (contador < qtd) {
+        cin >> altura >> base;
+
+        cout << inclinacao(altura, base) << endl;
+            
+        if (inclinacao(altura, base) <= 8.0) {
+            adequadas++;
+        }
+
+        contador++;
+    }
+
+    cout << adequadas << endl;
+    
+    return 0;
+}
