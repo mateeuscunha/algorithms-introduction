017d9048352f55e685f40ec5ffae04f889ec6afb
Mateus Cunha <mateuscunhazp@gmail.com>
2026-04-13 14:38:52 -0300

    correcting task 2

--- dredd/lista-while/task2/task2.cpp
+++ dredd/lista-while/task2/task2.cpp
@@ -2,6 +2,26 @@
 #include <fstream>
 using namespace std;
 
+int main() {
+    int N;
+    ifstream input ("entrada.txt");
+    input >> N;
+
+    ofstream output ("saida.txt");
+
+    int iteravel = 1;
+    int triangular = 0;
+    while(iteravel <= N) {
+        triangular += iteravel;
+        iteravel ++;
+    }
+
+    output << triangular << endl;
+
+}
+
+
+/*
 int triangular(int n) {
     int res = 0;
     res = ((n*(n+1)) / 2);
@@ -18,4 +38,4 @@ int main() {
     output << triangular(n) << endl;
 
     return 0;
-}
+} */
