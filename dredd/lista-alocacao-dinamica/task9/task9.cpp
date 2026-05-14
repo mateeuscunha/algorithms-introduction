889425c5a31c15572cbb207097cb031a80243eae
Mateus Cunha <mateuscunhazp@gmail.com>
2026-05-14 20:40:11 -0300

    trying to do task9

---
+++ dredd/lista-alocacao-dinamica/task9/task9.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int first;
+	cin >> first;
+	
+	char *primeiro = new char[first];
+	for (int i = 0; i < first; i++)
+		cin >> primeiro[i];
+	
+	int second;
+	cin >> second;
+	
+	char *segundo = new char[second];
+	for (int i = 0; i < second; i++)
+		cin >> segundo[i];
+	
+	int contador = 0;
+	for (int i = 0; i < first; i++) {
+		int j = 0;
+		bool sair = true;
+		while ((j < second) and (sair)) {
+			if (primeiro[i] == segundo[j]) {
+				primeiro[i] = -1;
+				sair = false;
+				contador++;
+			}
+			j++;
+		}
+	}
+	
+	char *res = new char[contador];
+	
+	int p = 0;
+	for (int i = 0; i < first; i++)
+		if (primeiro[i] != -1)
+			res[p++] = primeiro[i];
+
+	for (int i = 0; i < contador; i++)
+		cout << res[i] << " ";
+
+	
+	delete[] primeiro;
+	delete[] segundo;
+	delete[] res;
+	return 0;
+}
